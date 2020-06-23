041ece88736ddbf2c13b15c3e7585c57a7c84847
Yohan AVENEL <yohan.avenel@hotmail.com>
2020-06-23 18:38:00 +0200

    Changement config

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const {Client} = require('discord.js');
+const Discord = require('discord.js');
+const {TOKEN, PREFIX } = require("./config.js");
 const client = new Discord.Client({disableEveryone: true});
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("je suis Prêt");
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') msg.channel.send('Pong!');
-  if (msg.content === 'everyone') msg.channel.send('Salut a tous !', {disableEveryone: false });
-  if (msg.content === 'noteveryone') msg.channel.send(' @everyone (noteveryone), Salut tous le monde');
+  if (msg.content.startsWith(`$(PREFIX)ping`)) msg.channel.send("Pong!");
+  if (msg.content.startsWith(`$(PREFIX)pong`)) msg.channel.send("Ping!");    
 });
 
-client.login('NzI0MzIyNzI1MTQwODg5NjEx.XvIecA.Col9FBn-a3mmW2sbtONRKBpO9AM');
+client.login(TOKEN);
